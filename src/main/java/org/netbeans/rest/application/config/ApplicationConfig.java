e272621e30100d1b85b16f580c906f085086c8b1
Asantibo <asantibo129@gmail.com>
2021-11-02 13:02:32 -0500

    vistas base de datos agregadas

--- src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
+++ src/main/java/org/netbeans/rest/application/config/ApplicationConfig.java
@@ -29,7 +29,6 @@ public class ApplicationConfig extends Application {
      * If required, comment out calling this method in getClasses().
      */
     private void addRestResourceClasses(Set<Class<?>> resources) {
-        resources.add(co.edu.unicundi.proyectocdejb.exception.ExceptionHandler.class);
         resources.add(co.edu.unicundi.proyectodc.controller.CantanteController.class);
     }
     
