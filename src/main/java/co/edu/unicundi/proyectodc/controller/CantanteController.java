cd209793c61ea3c63a1f8a64e99aeb56e71ab741
David2813 <quinterosotodavidjavier@gmail.com>
2021-11-02 12:52:53 -0500

    cambios de servicio

--- src/main/java/co/edu/unicundi/proyectodc/controller/CantanteController.java
+++ src/main/java/co/edu/unicundi/proyectodc/controller/CantanteController.java
@@ -61,7 +61,8 @@ public class CantanteController {
     }
     @Path("actualizarCantante")
     @PUT
-    public Response actualizarCantante(){
+    public Response actualizarCantante(Cantante cantanteNuevosDatos){
+        this.service.actualizarCantante(cantanteNuevosDatos);
         return Response.status(Response.Status.OK).build();
     }
     @Path("cantantePorId/{idCantante}")
