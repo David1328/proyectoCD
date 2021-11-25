7ea3c4ca66bfc9575014850e0801ca042103007f
David2813 <quinterosotodavidjavier@gmail.com>
2021-11-25 00:17:24 -0500

    solucion errores

--- src/main/java/co/edu/unicundi/proyectodc/controller/DiscoController.java
+++ src/main/java/co/edu/unicundi/proyectodc/controller/DiscoController.java
@@ -7,6 +7,7 @@ package co.edu.unicundi.proyectodc.controller;
 
 
 import co.edu.unicundi.proyectocdejb.enity.Disco;
+import co.edu.unicundi.proyectocdejb.exception.RecursoNoEncontrado;
 import co.edu.unicundi.proyectocdejb.service.IDiscoService;
 import javax.ejb.EJB;
 import javax.ws.rs.Consumes;
@@ -40,7 +41,7 @@ public class DiscoController {
     
     @Path("agregarDisco")
     @POST
-    public Response agregarDisco(Disco nuevoDisco){
+    public Response agregarDisco(Disco nuevoDisco) throws RecursoNoEncontrado{
         System.out.println("Servicio para agregar Disco");
         this.service.agregarDisco(nuevoDisco);
         return Response.status(Response.Status.CREATED).build();
